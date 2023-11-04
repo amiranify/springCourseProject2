f3a93143ad216fef2229c73e28cde68ca91396fc
amiranify <amiranify@mail.ru>
2023-11-04 22:24:19 +0300

    first commit

---
+++ src/main/java/ru/amiranify/springcourse/dao/BookDAO.java
@@ -0,0 +1,8 @@
+package ru.amiranify.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
